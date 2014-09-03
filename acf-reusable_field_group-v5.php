cc0abce1d5943555460c2f227d22c490c3e15683
Tyler Bruffy <TyBruffy@gmail.com>
2014-09-03 17:06:14 -0400

    initial commit

---
+++ acf-reusable_field_group-v5.php
@@ -0,0 +1,586 @@
+<?php
+
+if( ! class_exists('acf_field_reusable_field_group') ) :
+
+class acf_field_reusable_field_group extends acf_field {
+	
+	
+	/*
+	*  __construct
+	*
+	*  This function will setup the field type data
+	*
+	*  @type	function
+	*  @date	5/03/2014
+	*  @since	5.0.0
+	*
+	*  @param	n/a
+	*  @return	n/a
+	*/
+	
+	function __construct() {
+		/*
+		*  name (string) Single word, no spaces. Underscores allowed
+		*/
+		
+		$this->name = 'reusable_field_group';
+		
+		
+		/*
+		*  label (string) Multiple words, can include spaces, visible when selecting a field type
+		*/
+		
+		$this->label = __('Reusable Field Group', 'acf-reusable_field_group');
+		
+		
+		/*
+		*  category (string) basic | content | choice | relational | jquery | layout | CUSTOM GROUP NAME
+		*/
+		
+		$this->category = 'Relational';
+		
+		
+		/*
+		*  defaults (array) Array of default settings which are merged into the field object. These are used later in settings
+		*/
+		
+		$this->defaults = array(
+			'group_key'	=> 0,
+		);
+		
+		
+		/*
+		*  l10n (array) Array of strings that are used in JavaScript. This allows JS strings to be translated in PHP and loaded via:
+		*  var message = acf._e('reusable_field_group', 'error');
+		*/
+		
+		$this->l10n = array(
+			'error'	=> __('Error! Please select a field group', 'acf-reusable_field_group'),
+		);
+
+		add_filter("acf/get_field_types",								array($this, 'get_field_types'), 10, 1);
+		
+				
+		// do not delete!
+    	parent::__construct();
+    	
+	}
+	
+	
+	/*
+	*  render_field_settings()
+	*
+	*  Create extra settings for your field. These are visible when editing a field
+	*
+	*  @type	action
+	*  @since	3.6
+	*  @date	23/01/13
+	*
+	*  @param	$field (array) the $field being edited
+	*  @return	n/a
+	*/
+	
+	function render_field_settings( $field ) {
+		acf_render_field_setting( $field, array(
+			'label'			=> __('Field Group','acf'),
+			'instructions'	=> '',
+			'type'			=> 'select',
+			'name'			=> 'group_key',
+			'choices'		=> $this->pretty_field_groups($field),
+			'multiple'		=> 0,
+			'ui'			=> 1,
+			'allow_null'	=> 0,
+			'placeholder'	=> __("No Field Group",'acf'),
+		));
+	}
+
+
+	function pretty_field_groups($field) {
+		global $post;
+		
+		$groups = acf_get_field_groups();
+		$r      = array();
+
+		$current_id = $post->ID ? $post->ID : $_POST["parent"];
+		$current_group = _acf_get_field_group_by_id($current_id);
+
+		foreach( $groups as $group ) {
+			$key = $group["key"];
+
+			// don't show the current field group.
+			if ($key == $current_group["key"]) {
+				continue;
+			}
+
+			$r[$key] = $group["title"];
+		}
+		
+		return $r;
+	}
+
+
+	/*
+	*  render_field()
+	*
+	*  Create the HTML interface for your field
+	*
+	*  @param	$field (array) the $field being rendered
+	*
+	*  @type	action
+	*  @since	3.6
+	*  @date	23/01/13
+	*
+	*  @param	$field (array) the $field being edited
+	*  @return	n/a
+	*/
+	
+	function render_field( $field ) {
+		global $post;
+		$current_id = $post->ID;
+		
+		$group  = _acf_get_field_group_by_key($field["group_key"]);
+		$fields = acf_get_fields($group);
+
+		acf_render_fields($current_id, $fields);
+	}
+	
+		
+	/*
+	*  input_admin_enqueue_scripts()
+	*
+	*  This action is called in the admin_enqueue_scripts action on the edit screen where your field is created.
+	*  Use this action to add CSS + JavaScript to assist your render_field() action.
+	*
+	*  @type	action (admin_enqueue_scripts)
+	*  @since	3.6
+	*  @date	23/01/13
+	*
+	*  @param	n/a
+	*  @return	n/a
+	*/
+
+	/*
+	
+	function input_admin_enqueue_scripts() {
+		
+		$dir = plugin_dir_url( __FILE__ );
+		
+		
+		// register & include JS
+		wp_register_script( 'acf-input-reusable_field_group', "{$dir}js/input.js" );
+		wp_enqueue_script('acf-input-reusable_field_group');
+		
+		
+		// register & include CSS
+		wp_register_style( 'acf-input-reusable_field_group', "{$dir}css/input.css" ); 
+		wp_enqueue_style('acf-input-reusable_field_group');
+		
+		
+	}
+	
+	*/
+	
+	
+	/*
+	*  input_admin_head()
+	*
+	*  This action is called in the admin_head action on the edit screen where your field is created.
+	*  Use this action to add CSS and JavaScript to assist your render_field() action.
+	*
+	*  @type	action (admin_head)
+	*  @since	3.6
+	*  @date	23/01/13
+	*
+	*  @param	n/a
+	*  @return	n/a
+	*/
+
+	/*
+		
+	function input_admin_head() {
+	
+		
+		
+	}
+	
+	*/
+	
+	
+	/*
+   	*  input_form_data()
+   	*
+   	*  This function is called once on the 'input' page between the head and footer
+   	*  There are 2 situations where ACF did not load during the 'acf/input_admin_enqueue_scripts' and 
+   	*  'acf/input_admin_head' actions because ACF did not know it was going to be used. These situations are
+   	*  seen on comments / user edit forms on the front end. This function will always be called, and includes
+   	*  $args that related to the current screen such as $args['post_id']
+   	*
+   	*  @type	function
+   	*  @date	6/03/2014
+   	*  @since	5.0.0
+   	*
+   	*  @param	$args (array)
+   	*  @return	n/a
+   	*/
+   	
+   	/*
+   	
+   	function input_form_data( $args ) {
+	   	
+		
+	
+   	}
+   	
+   	*/
+	
+	
+	/*
+	*  input_admin_footer()
+	*
+	*  This action is called in the admin_footer action on the edit screen where your field is created.
+	*  Use this action to add CSS and JavaScript to assist your render_field() action.
+	*
+	*  @type	action (admin_footer)
+	*  @since	3.6
+	*  @date	23/01/13
+	*
+	*  @param	n/a
+	*  @return	n/a
+	*/
+
+	/*
+		
+	function input_admin_footer() {
+	
+		
+		
+	}
+	
+	*/
+	
+	
+	/*
+	*  field_group_admin_enqueue_scripts()
+	*
+	*  This action is called in the admin_enqueue_scripts action on the edit screen where your field is edited.
+	*  Use this action to add CSS + JavaScript to assist your render_field_options() action.
+	*
+	*  @type	action (admin_enqueue_scripts)
+	*  @since	3.6
+	*  @date	23/01/13
+	*
+	*  @param	n/a
+	*  @return	n/a
+	*/
+
+	/*
+	
+	function field_group_admin_enqueue_scripts() {
+		
+	}
+	
+	*/
+
+	
+	/*
+	*  field_group_admin_head()
+	*
+	*  This action is called in the admin_head action on the edit screen where your field is edited.
+	*  Use this action to add CSS and JavaScript to assist your render_field_options() action.
+	*
+	*  @type	action (admin_head)
+	*  @since	3.6
+	*  @date	23/01/13
+	*
+	*  @param	n/a
+	*  @return	n/a
+	*/
+
+	
+	
+	function field_group_admin_head() {
+		?>
+		<style type="text/css">
+
+		.acf-field-list .field_type-reusable_field_group tr[data-name="name"], 
+		.acf-field-list .field_type-reusable_field_group tr[data-name="instructions"], 
+		.acf-field-list .field_type-reusable_field_group tr[data-name="required"], 
+		.acf-field-list .field_type-reusable_field_group tr[data-name="warning"] {
+			display: none !important;
+		}
+
+		</style>
+		<script type="text/javascript">
+			acf.add_action('change_field_type', function( $el ){
+				var	type	= $el.attr('data-type');
+				
+				//  Remove field name
+				if ( type == 'reusable_field_group' ) {
+					$el.find('tr[data-name="name"] input').val('').trigger('keyup');
+				}
+			});
+
+			acf.add_action('change_field_label', function( $el ){
+				var $label = $el.find('tr[data-name="label"]:first input'),
+					$name  = $el.find('tr[data-name="name"]:first input'),
+					type   = $el.attr('data-type');
+
+				if ( type == 'reusable_field_group' ) {
+					$name.val('').trigger('change');
+					$el.find('> .field-info .li-field_name').text('');
+					return;
+				}
+			});
+		</script>
+			<?php
+	}
+	
+	
+
+
+	/*
+	*  load_value()
+	*
+	*  This filter is applied to the $value after it is loaded from the db
+	*
+	*  @type	filter
+	*  @since	3.6
+	*  @date	23/01/13
+	*
+	*  @param	$value (mixed) the value found in the database
+	*  @param	$post_id (mixed) the $post_id from which the value was loaded
+	*  @param	$field (array) the field array holding all the field options
+	*  @return	$value
+	*/
+	
+	/*
+	
+	function load_value( $value, $post_id, $field ) {
+		
+		return $value;
+		
+	}
+	
+	*/
+	
+	
+	/*
+	*  update_value()
+	*
+	*  This filter is applied to the $value before it is saved in the db
+	*
+	*  @type	filter
+	*  @since	3.6
+	*  @date	23/01/13
+	*
+	*  @param	$value (mixed) the value found in the database
+	*  @param	$post_id (mixed) the $post_id from which the value was loaded
+	*  @param	$field (array) the field array holding all the field options
+	*  @return	$value
+	*/
+	
+	
+	/*	
+	function update_value( $value, $post_id, $field ) {
+
+		return $value;
+		
+	}
+	*/
+	
+	
+	/*
+	*  format_value()
+	*
+	*  This filter is appied to the $value after it is loaded from the db and before it is returned to the template
+	*
+	*  @type	filter
+	*  @since	3.6
+	*  @date	23/01/13
+	*
+	*  @param	$value (mixed) the value which was loaded from the database
+	*  @param	$post_id (mixed) the $post_id from which the value was loaded
+	*  @param	$field (array) the field array holding all the field options
+	*
+	*  @return	$value (mixed) the modified value
+	*/
+		
+	/*
+	
+	function format_value( $value, $post_id, $field ) {
+		
+		// bail early if no value
+		if( empty($value) ) {
+		
+			return $value;
+			
+		}
+		
+		
+		// apply setting
+		if( $field['font_size'] > 12 ) { 
+			
+			// format the value
+			// $value = 'something';
+		
+		}
+		
+		
+		// return
+		return $value;
+	}
+	
+	*/
+	
+	
+	/*
+	*  validate_value()
+	*
+	*  This filter is used to perform validation on the value prior to saving.
+	*  All values are validated regardless of the field's required setting. This allows you to validate and return
+	*  messages to the user if the value is not correct
+	*
+	*  @type	filter
+	*  @date	11/02/2014
+	*  @since	5.0.0
+	*
+	*  @param	$valid (boolean) validation status based on the value and the field's required setting
+	*  @param	$value (mixed) the $_POST value
+	*  @param	$field (array) the field array holding all the field options
+	*  @param	$input (string) the corresponding input name for $_POST value
+	*  @return	$valid
+	*/
+	
+	/*
+	
+	function validate_value( $valid, $value, $field, $input ){
+		
+		// Basic usage
+		if( $value < $field['custom_minimum_setting'] )
+		{
+			$valid = false;
+		}
+		
+		
+		// Advanced usage
+		if( $value < $field['custom_minimum_setting'] )
+		{
+			$valid = __('The value is too little!','acf-reusable_field_group'),
+		}
+		
+		
+		// return
+		return $valid;
+		
+	}
+	
+	*/
+	
+	
+	/*
+	*  delete_value()
+	*
+	*  This action is fired after a value has been deleted from the db.
+	*  Please note that saving a blank value is treated as an update, not a delete
+	*
+	*  @type	action
+	*  @date	6/03/2014
+	*  @since	5.0.0
+	*
+	*  @param	$post_id (mixed) the $post_id from which the value was deleted
+	*  @param	$key (string) the $meta_key which the value was deleted
+	*  @return	n/a
+	*/
+	
+	/*
+	
+	function delete_value( $post_id, $key ) {
+		
+		
+		
+	}
+	
+	*/
+	
+	
+	/*
+	*  load_field()
+	*
+	*  This filter is applied to the $field after it is loaded from the database
+	*
+	*  @type	filter
+	*  @date	23/01/2013
+	*  @since	3.6.0	
+	*
+	*  @param	$field (array) the field array holding all the field options
+	*  @return	$field
+	*/
+	
+	/*
+	
+	function load_field( $field ) {
+		
+		return $field;
+		
+	}	
+	
+	*/
+	
+	
+	/*
+	*  update_field()
+	*
+	*  This filter is applied to the $field before it is saved to the database
+	*
+	*  @type	filter
+	*  @date	23/01/2013
+	*  @since	3.6.0
+	*
+	*  @param	$field (array) the field array holding all the field options
+	*  @return	$field
+	*/
+	
+	/*
+	
+	function update_field( $field ) {
+		
+		return $field;
+		
+	}	
+	
+	*/
+	
+	
+	/*
+	*  delete_field()
+	*
+	*  This action is fired after a field is deleted from the database
+	*
+	*  @type	action
+	*  @date	11/02/2014
+	*  @since	5.0.0
+	*
+	*  @param	$field (array) the field array holding all the field options
+	*  @return	n/a
+	*/
+	
+	/*
+	
+	function delete_field( $field ) {
+		
+		
+		
+	}	
+	
+	*/
+	
+	
+}
+
+
+// create field
+new acf_field_reusable_field_group();
+
+endif;
+
+?>
---
+++ readme.txt
@@ -0,0 +1,32 @@
+=== Advanced Custom Fields: Reusable Field Group Field ===
+Contributors: Tyler Bruffy
+Tags: PLUGIN_TAGS
+Requires at least: 3.5
+Tested up to: 3.8.1
+Stable tag: trunk
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Include an existing ACF Field Group in the template for another Field Group
+
+== Include an existing ACF Field Group in the template for another Field Group ==
+
+EXTENDED_Include an existing ACF Field Group in the template for another Field Group
+
+= Compatibility =
+
+This ACF field type is compatible with:
+* ACF 5
+* ACF 4
+
+== Installation ==
+
+1. Copy the `acf-reusable_field_group` folder into your `wp-content/plugins` folder
+2. Activate the Reusable Field Group plugin via the plugins admin page
+3. Create a new field via ACF and select the Reusable Field Group type
+4. Please refer to the Include an existing ACF Field Group in the template for another Field Group for more info regarding the field type settings
+
+== Changelog ==
+
+= 1.0.0 =
+* Initial Release.
